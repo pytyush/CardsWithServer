05ba9f9d6c0307d42d70c21d76c427cb5df4951e
pytyush <pratyush11197@gmail.com>
2018-01-04 20:46:44 +0530

    Initial commit

---
+++ CustomAdapter.java
@@ -0,0 +1,52 @@
+package com.example.pratyush_pr.cardswithserver;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+/**
+ * Created by Pratyush_PR on 11/9/2017.
+ */
+
+public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder> {
+    private Context context;
+    private List<MyData> my_data;
+    public CustomAdapter(Context context, List<MyData> my_data) {
+        this.context=context;
+        this.my_data=my_data;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView= LayoutInflater.from(parent.getContext()).inflate(R.layout.card_view,parent,false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.description.setText(my_data.get(position).getDescription());
+        Glide.with(context).load(my_data.get(position).getImage_link()).into(holder.imageView);
+    }
+    @Override
+    public int getItemCount() {
+        return my_data.size();
+    }
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView description;
+        public ImageView imageView;
+
+        public ViewHolder(View itemView){
+            super(itemView);
+            description=(TextView)itemView.findViewById(R.id.text);
+            imageView=(ImageView)itemView.findViewById(R.id.image);
+        }
+    }
+}
